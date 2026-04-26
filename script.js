fef5da7d64510ff6d275b07fd1b3048992b002e4
Tapan Rathore <tapanrathore07@gmail.com>
2026-04-26 20:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+// console.log(squares);
+squares.forEach(square=>{
+	square.addEventListener("mouseenter", ()=>{
+		squares.forEach(sq=>{
+			sq.style.backgroundColor="#6F4E37" //coffe
+		});
+		square.style.backgroundColor="#E6E6FA" //normal color
+	});
+
+	square.addEventListener("mouseleave", ()=>{
+		squares.forEach(sq=>{
+			sq.style.backgroundColor="#E6E6FA" //resest to orginal
+		})
+	})
+})
